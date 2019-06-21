c9334d4aab9c46c9064dd57f1dd8cb5a948cff63
rfrieger <rfrieger@gmail.com>
2019-06-21 14:40:00 -0400

    add remove size done

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,80 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.security.PublicKey;
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node head;
+    Node tail;
+    int size = 0;
+
+    public void add(Integer info) {
+        size++;
+        if (head == null) {
+            head = new Node(info, null, null);
+            tail = head;
+        } else {
+            Node node = new Node(info, head, null);
+            this.head.prev = node;
+            this.head = node;
+        }
+    }
+
+    public void remove(Integer info) {
+
+        if (head != null) {
+            size--;
+            if (tail.prev != null) {
+                tail.prev.next = null;
+                tail = tail.prev;
+            } else {
+                head = null;
+                tail = null;
+            }
+        }
+    }
+
+    public Integer size() {
+        return this.size;
+    }
+
+
+    public class Node {
+        private Node next;
+        private Node prev;
+        private Integer data;
+
+    public Node(Integer data, Node next, Node prev) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    public void setInfo(Integer data) {
+        this.data = data;
+    }
+
+    public void setNext(Node node) {
+        next = node;
+    }
+
+    public void setPrev(Node node) {
+        prev = node;
+    }
+
+    public Integer getInfo() {
+        return data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+}
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,36 @@
 package com.zipcodewilmington.singlylinkedlist;
 
-/**
- * Created by leon on 1/10/18.
- */
+import org.junit.Assert;
+import org.junit.Test;
+
 public class SinglyLinkedListTest {
+
+    @Test
+    public void add() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(5);
+        singlyLinkedList.add(6);
+
+        Integer actual = singlyLinkedList.size;
+        Integer expected = 2;
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void remove() {
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        singlyLinkedList.add(5);
+        singlyLinkedList.add(6);
+        singlyLinkedList.remove(6);
+
+        Integer actual = singlyLinkedList.size;
+        Integer expected = 1;
+
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void size() {
+    }
 }
